c81ca87030fea492790a4924310196e7e5a225dc
Taylor Yip <63479174+yippptay@users.noreply.github.com>
2023-01-19 16:44:23 -0800

    i have no idea

--- assets/predictive-search.js
+++ assets/predictive-search.js
@@ -128,7 +128,7 @@ class PredictiveSearch extends HTMLElement {
       return;
     }
 
-    fetch(`${routes.predictive_search_url}?q=${encodeURIComponent(searchTerm)}&${encodeURIComponent('resources[type]')}=product&${encodeURIComponent('resources[limit]')}=4&section_id=predictive-search`)
+    fetch(`${routes.predictive_search_url}?q=${encodeURIComponent(searchTerm)}&${encodeURIComponent('resources[type]')}=product&${encodeURIComponent('resources[limit]')}=8&section_id=predictive-search`)
       .then((response) => {
         if (!response.ok) {
           var error = new Error(response.status);
